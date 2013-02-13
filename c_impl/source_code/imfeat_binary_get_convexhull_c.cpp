d6f79b6f99528b49923f17068efcd31d1032837b
Chung-Ta Ku <ctku@lamppc41>
2013-02-13 17:40:49 -0500

    Convex Hull v2 (strange)

--- c_impl/source_code/imfeat_binary_get_convexhull_c.cpp
+++ c_impl/source_code/imfeat_binary_get_convexhull_c.cpp
@@ -28,187 +28,170 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <math.h>
 
-#define min(X,Y)	((X) < (Y) ? (X) : (Y))
-#define max(X,Y)	((X) > (Y) ? (X) : (Y))
+typedef struct 
+{
+        double x;
+        double y;
+} point_t;
 
-struct point {
-	int x;
-	int y;
-};
+int jarvis(point_t *cloud, int n);
+int graham(point_t *cloud, int n);
 
-typedef point p2d_t;
+static point_t base;
 
-/* Stack of points */
-struct p_node {
-	p2d_t point;
-	p_node* next;
-};
+int is_inside_segment (point_t *p0, point_t *p1, point_t *p2)
+{
+	if ((p1->x < p2->x) && (p0->x >= p1->x) && (p0->x <= p2->x)) return 1;
+	if ((p1->x > p2->x) && (p0->x <= p1->x) && (p0->x >= p2->x)) return 1;
+	if ((p1->y < p2->y) && (p0->y >= p1->y) && (p0->y <= p2->y)) return 1;
+	if ((p1->y > p2->y) && (p0->y <= p1->y) && (p0->y >= p2->y)) return 1;
 
-typedef struct p_node p_node_t;
-
-struct stack {
-	int size;
-	p_node_t* top;
-	p_node_t* bottom;
-};
-
-typedef struct stack p_stack_t;
-
-p_stack_t* s_init() {
-	p_stack_t* newStack = (p_stack_t*)malloc(sizeof(*newStack));
-	newStack->top = newStack->bottom = NULL;
-	newStack->size = 0;
-	return newStack;
-}	
-
-p2d_t* pop(p_stack_t* stack) {
-	if(!stack->size)
-		return NULL;
-	p_node* temp = stack->top;
-	p2d_t* toReturn = (p2d_t*)malloc(sizeof(*toReturn));
-	*toReturn = stack->top->point;
-	stack->top = stack->top->next;
-	free(temp);
-	stack->size--;
-	return toReturn;
+    return 0;
 }
 
-void push(p_stack_t* stack, p2d_t point) {
-	p_node_t* temp;
-	if(stack->top) {
-		temp = stack->top;
-		stack->top = (p_node_t*)malloc(sizeof(p_node_t));
-		stack->top->point = point;
-		stack->top->next = temp;
-	} else {
-		stack->top = stack->bottom = (p_node_t*)malloc(sizeof(p_node_t));
-		stack->top->next = NULL;
-		stack->top->point = point;
+double ccw(point_t *p1, point_t *p2, point_t *p3)
+{
+	double v = (p2->x - p1->x)*(p3->y - p1->y) - (p3->x - p1->x)*(p2->y - p1->y);
+	return v;
+}
+
+void swap (point_t *cloud, int i, int j)
+{
+	point_t tmp;
+
+	tmp.x = (cloud+i)->x; tmp.y = (cloud+i)->y;
+	(cloud+i)->x = (cloud+j)->x;
+	(cloud+i)->y = (cloud+j)->y;
+	(cloud+j)->x = tmp.x;
+	(cloud+j)->y = tmp.y;
+}
+
+int compar_cotan(const void *a, const void *b)
+{
+	point_t *p1 = (point_t *)a;
+	point_t *p2 = (point_t *)b;
+	double cotan1, cotan2;
+
+	if ( (p1->x == base.x) && (p1->y == base.y) ) return -1;
+	if ( (p2->x == base.x) && (p2->y == base.y) ) return 1;
+
+	/* p1, p2 and base are on the same vertical line */
+	if ( (p1->x == base.x) && (p2->x == base.x) ) {
+		if (p1->y < p2->y) return -1;
+		return 1;
 	}
-	stack->size++;
-}
 
-p2d_t* top(p_stack_t* stack) {
-	if(!stack->size)
-		return NULL;
-	return &stack->top->point;
-}
-
-p2d_t* next_to_top(p_stack_t* stack) {
-	if(stack->size<2)
-		return NULL;
-	return &stack->top->next->point;
-}
-
-/*
- *	Determine the convex hull of a set of points
- *		The points are in the plane
- */
-
-/* positive, pj is left of [pi, pk] */
-float direction(p2d_t pi, p2d_t pj, p2d_t pk) {
-	return (pj.x-pi.x)*(pk.y-pi.y) - (pk.x-pi.x)*(pj.y-pi.y);
-}
-/* Returns 1 if pk, known to be colinear with pi and pj, is actually on the segment [pi...[j] */
-char on_segment(p2d_t pi, p2d_t pj, p2d_t pk) {
-	if(min(pi.x, pj.x)<=pk.x<=max(pi.x, pj.x) &&
-	   min(pi.y, pj.y)<=pk.y<=max(pi.y, pj.y))
+	/* p1, p2 and base are on the same horizontal line 
+	   per Graham algo, base has the lowest x-coordinate. 
+	   Sort p1 and p2 according to the lowest x-coordinate */
+	if ( (p1->y == base.y) && (p2->y == base.y) ) {
+		if (p1->x < p2->x) return -1;
 		return 1;
-	return 0;
-}
-/* Return 1 if [p1, p2] intersects [p3, p4] */
-char intersection(p2d_t p1, p2d_t p2, p2d_t p3, p2d_t p4) {
-	float d1 = direction(p3, p4, p1);
-	float d2 = direction(p3, p4, p2);
-	float d3 = direction(p1, p2, p3);
-	float d4 = direction(p1, p2, p4);
-	if((d1>0 && d2<0) ||
-	   (d1<0 && d2>0) ||
-	   (d3>0 && d4<0) ||
-	   (d3<0 && d4>0))
-		return 1;
-	if(d1==0 && on_segment(p3, p4, p1))
-		return 1;
-	if(d2==0 && on_segment(p3, p4, p2))
-		return 1;
-	if(d3==0 && on_segment(p1, p2, p3))
-		return 1;
-	if(d4==0 && on_segment(p1, p2, p4))
-		return 1;
-	return 0;
-}
-/* Sort points[p...r] by the polar angle made with origin p0 */
-p2d_t* polar_angle_sort(p2d_t* p0, p2d_t* points, int p, int r) {
-	int i=0, j=0, k=0;
-	int piv; 
-	if(r-p<=1)
-		return 0;
-	p2d_t* g = (p2d_t*)malloc((r-p+1)*sizeof(*g));
-	p2d_t* l = (p2d_t*)malloc((r-p+1)*sizeof(*l));
-	float v1 = direction(*p0, points[p], points[(int)floor(float(p+r)/2)]);
-	float v2 = direction(*p0, points[(int)floor(float(p+r)/2)], points[r]);
-	float v3 = direction(*p0, points[p], points[r]);
-	piv = v1>v2?(v2>v3?p:r):(v2>v3?floor(float(p+r)/2):r);
-	p2d_t val_piv = points[piv-1];
-	for(i=p;i<=r;i++) {
-		if(i==piv)
-			continue;
-		if(direction(*p0, val_piv, points[i])>0)
-			l[j++] = points[i];
-		else
-			g[k++] = points[i];
 	}
-	l = polar_angle_sort(p0, l, 0, j);
-	g = polar_angle_sort(p0, g, 0, r);
-	for(i=0;i<j;i++)
-		points[i] = l[i];
-	points[i] = val_piv;
-	for(i=0;i<k;i++)
-		points[j+i] = g[i];
-	return points;
-}
 
-void convex_hull(p2d_t* points, int n, p2d_t* hull, int* hull_size) {
-	int i;
-	int min_y = 100, min_pos=0;
-	for(i=0;i<n;i++)
-		if(points[i].y<min_y) {
-			min_y = points[i].y;
-			min_pos = i;
+	cotan1 = (p1->x - base.x)/(p1->y - base.y);
+	cotan2 = (p2->x - base.x)/(p2->y - base.y);
+
+	if (cotan1 > cotan2) return -1;
+	if (cotan1 < cotan2) return  1;
+	if (cotan1 == cotan2) { /* both points are aligned on the same side of base point - closest point is the first */
+		if (cotan1 < 0) {   /* Angle > 90 */
+			if (p1->x < p2->x) return -1;
+			if (p2->x < p1->x) return 1;
 		}
-	p2d_t p0 = points[min_pos];
-	for(i=min_pos;i<n-1;i++)
-		points[i] = points[i+1];
-	n--;
-	points = polar_angle_sort(&p0, points, 0, n);
-	p_stack_t* qPoints = s_init();
-	push(qPoints, p0);
-	push(qPoints, points[0]);
-	push(qPoints, points[1]);
-	for(i=2;i<n;i++) {
-		while(direction(*next_to_top(qPoints), *top(qPoints), points[i])<0)
-			pop(qPoints);
-		push(qPoints, points[i]);
+		if (cotan1 > 0) { /* Angle < 90 */
+			if (p1->x < p2->x) return 1;
+			if (p2->x < p1->x) return -1;
+		}
 	}
-	p2d_t* point;
-	*hull_size = 0;
-	while(point = pop(qPoints))
-		hull[*(hull_size++)] = *point;
+
+	return 0;
 }
+
+int jarvis(point_t *cloud, int n)
+{
+	int i, b=0, a=0, minx, np;
+
+	minx = cloud[0].x;
+	np = 1;
+	for (i=1; i < n; i++)
+	{
+		if (cloud[i].x < minx) {
+			minx = cloud[i].x;
+			a = i;
+		}
+	}
+	swap(cloud, a, 0);
+	while(1) {
+		a = np-1;
+		if ( a == 0 ) {
+			b = 1;
+			for (i=2; i < n; i++)
+				if (ccw(cloud+a, cloud+b, cloud+i) > 0) b = i;
+				else if (ccw(cloud+a, cloud+b, cloud+i) == 0) {
+					/* If the 3 points are aligned, update b only if i is between a and b */
+					if (is_inside_segment(cloud+i, cloud+a, cloud+b))
+						b = i;
+				}		
+		}
+		else {
+			b = 0;
+			for (i=1; i <n; i++) {
+				if (i == a) continue;
+				if (ccw(cloud+a, cloud+b, cloud+i) > 0) b = i;
+				else if (ccw(cloud+a, cloud+b, cloud+i) == 0) {
+					if (is_inside_segment(cloud+i, cloud+a, cloud+b))
+						b = i;
+				}		
+			}
+		}
+		if (b == 0) break;
+		swap(cloud, np++, b);
+	}
+	return np;
+}
+
+int graham (point_t *cloud, int n)
+{
+	int i, a0, m;
+
+	a0 = 0;
+	for (i=1; i < n; i++) {
+		if (cloud[a0].y > cloud[i].y)
+			a0 = i;
+		if (cloud[a0].y == cloud[i].y)
+			if (cloud[a0].x > cloud[i].x)
+				a0 = i;
+	}	
+	base.x = cloud[a0].x; base.y = cloud[a0].y;
+	cloud[n].x = base.x; cloud[n].y = base.y;
+	qsort(cloud, n+1, sizeof(point_t), compar_cotan);
+	cloud[0].x = cloud[n].x; cloud[0].y = cloud[n].y;
+
+	m = 2;
+	for (i=3; i<=n; i++) {
+		while (ccw(cloud+m-1, cloud+m, cloud+i) <= 0)
+			m--;
+		m++;
+		swap(cloud, m, i);
+	}
+	/* Swap back the last point to avoid corrupting the initial set */
+	swap(cloud, m, n);
+	return m;
+}
+
 
 #ifndef MATLAB
 int main(void)
 {
-	u8 img_cum[15] = {0,0,1,0,0,
-                      0,0,1,0,0,
-					  1,1,1,1,1};
-	int w = 5, h = 3;
-	p2d_t *points = (p2d_t*)malloc(sizeof(p2d_t *)*w*h);
-	p2d_t *hull = (p2d_t*)malloc(sizeof(p2d_t *)*w*h);
-	memset(points, 0, sizeof(p2d_t *)*w*h);
-	memset(hull, 0, sizeof(p2d_t *)*w*h);
+	u8 img_cum[21] = {0,0,0,1,0,0,0,
+                      0,0,0,0,0,0,0,
+					  0,1,0,0,0,1,0};
+	int w = 7, h = 3;
+	point_t *points = (point_t*)malloc(sizeof(point_t *)*w*h);
+	memset(points, 0, sizeof(point_t)*w*h);
+
 	int n = 0;
 	for (int i=0; i<h; i++) {
 		for (int j=0; j<w; j++) {
@@ -220,7 +203,7 @@ int main(void)
 		}
 	}
 	int hull_size = 0;
-	convex_hull(points, n, hull, &hull_size);
+	int m = jarvis(points, n);
 
 	return 0;
 }
